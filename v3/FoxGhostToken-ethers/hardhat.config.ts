741d3ee08d1da29c727d2c8fd9e4124edecfd97e
FoxGhost <63149175+FoxGhost@users.noreply.github.com>
2025-10-01 13:06:21 +0200

    add FoxGhostToken ethers deployment

---
+++ v3/FoxGhostToken-ethers/hardhat.config.ts
@@ -0,0 +1,61 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import hardhatNetworkHelpers from "@nomicfoundation/hardhat-network-helpers";
+
+
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxMochaEthersPlugin, hardhatNetworkHelpers],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.30",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("PRIVATE_KEY")],
+    },
+    links: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("LINKS_RPC_URL"),
+      accounts: [configVariable("PRIVATE_KEY")],
+      chainId: 1074,
+    },
+  },
+  chainDescriptors: {
+    // LINKS chain
+    1074: {
+      name: "links",
+      blockExplorers: {
+        blockscout: {
+          name: "Blockscout-LINKS",
+          url: "https://explorer.tangle.stardust.linksfoundation.com",
+          apiUrl: "https://explorer.tangle.stardust.linksfoundation.com/api",
+        },
+      },
+    },
+  },
+};
+
+export default config;
