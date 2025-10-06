9b594a520a2a1c995e028b9356b793eca7cfbeed
FoxGhost <63149175+FoxGhost@users.noreply.github.com>
2025-10-06 17:07:08 +0200

    replace `networkHelpers.loadFixture` with `deployContract` in FoxGhostToken-viem tests to avoid test interference caused by parallel execution

--- v3/FoxGhostToken-viem/test/FoxGhostToken.ts
+++ v3/FoxGhostToken-viem/test/FoxGhostToken.ts
@@ -7,14 +7,29 @@ import { getAddress } from "viem";
 import { get } from "node:http";
 const { networkHelpers } = await hre.network.connect();
 
+/* Fixtures
+* * fixtures can be used importing
+* * const { networkHelpers } = await hre.network.connect();
+* * and then await networkHelpers.loadFixture(fixtureName);
+* * but with hardhat 3 and Node.js test runner the tests run in parallel.
+* * So the test even if starting from the same state thanks to the fixture,
+* * are also using the same contract instance (due to the fixture behavior) so they influence each other
+* * because they run in parallel.
+* *
+* * If this code is run using fixtures, some tests will fail because the previous test influences the following one
+* * due to the parallelism. This means that the fixture works correctly, but then the tests
+* * that are running on the same contract instance are influencing each other
+* */
+
 describe("FoxGhostToken", async function () {
   const { viem } = await network.connect();
   const publicClient = await viem.getPublicClient();
 
   const totalSupply = BigInt(1000);
   const value = BigInt(5)
-//TODO fixture is now working fix the test accordingly
-  async function deployTokenFixture() {
+
+
+  async function deployContract() {
     const [owner, addr1, addr2] = await viem.getWalletClients();
 
     const Token = await viem.deployContract("FoxGhostToken", [owner.account.address]);
@@ -26,68 +41,68 @@ describe("FoxGhostToken", async function () {
   }
 
   it('Test Name', async function(){
-    const { Token: token } = await networkHelpers.loadFixture(deployTokenFixture);
+    const { Token: token } = await deployContract();
     assert.equal(await token.read.name(), "FoxGhostToken");
 
   });
 
   it('Test Symbol', async function(){
-    const { Token } = await networkHelpers.loadFixture(deployTokenFixture);
+    const { Token } = await deployContract();
     assert.equal(await Token.read.symbol(), "FGT");
   });
 
   it('Test Decimals', async function(){
-        const { Token } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token } = await deployContract();
         assert.equal(await Token.read.decimals(), 18);
     });
 
     it('Total Supply', async function(){
-        const { Token } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token } = await deployContract();
         assert.equal(await Token.read.totalSupply(), totalSupply);
     });
 
 
     it('Test Mint: Balance of the deployer', async function(){
-        const { Token, owner } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner } = await deployContract();
         assert.equal(await Token.read.balanceOf([getAddress(owner.account.address)]), totalSupply);
     });
 
     it('Test Mint: Mint to a user', async function(){
-        const { Token, owner, addr1 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1 } = await deployContract();
         await Token.write.mint([getAddress(addr1.account.address), value], {account: owner.account});
 
         assert.equal(await Token.read.balanceOf([addr1.account.address]), value);
     });
 
     it('Test Transfer: Owner pays user', async function(){
-        const { Token, owner, addr1 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1 } = await deployContract();
         await Token.write.transfer([getAddress(addr1.account.address), value], {account: owner.account});
         assert.equal(await Token.read.balanceOf([getAddress(addr1.account.address)]), value);
     });
 
     it('Test Transfer: User pays user', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.transfer([getAddress(addr1.account.address), value], {account: owner.account});
         await Token.write.transfer([getAddress(addr2.account.address), value], {account: addr1.account.address});
         assert.equal(await Token.read.balanceOf([getAddress(addr2.account.address)]), value);
     });
 
     it('Test Burn', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.transfer([getAddress(addr1.account.address), value], {account: owner.account});
         await Token.write.burn([value], {account: addr1.account});
-        await assert.equal(await Token.read.balanceOf([getAddress(addr1.account.address)]), BigInt(0));
+        assert.equal(await Token.read.balanceOf([getAddress(addr1.account.address)]), BigInt(0));
     });
 
     it('Test Approve: spend all in one', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.approve([getAddress(addr1.account.address), value], {account: owner.account});
         await Token.write.transferFrom([getAddress(owner.account.address), getAddress(addr2.account.address), value], {account: addr1.account});
         assert.equal(await Token.read.balanceOf([getAddress(addr2.account.address)]), value);
     });
 
     it('Test Approve: spend all in many', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.approve([getAddress(addr1.account.address), value], {account: owner.account});
 
         await Token.write.transferFrom([getAddress(owner.account.address), getAddress(addr2.account.address), BigInt(1)], {account: addr1.account});
@@ -100,7 +115,7 @@ describe("FoxGhostToken", async function () {
     });
 
     it('Test Approve: spend not all', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.approve([getAddress(addr1.account.address), value], {account: owner.account});
 
         await Token.write.transferFrom([getAddress(owner.account.address), getAddress(addr2.account.address), BigInt(1)], {account: addr1.account});
@@ -111,7 +126,7 @@ describe("FoxGhostToken", async function () {
     });
 
     it('Test Approve: spend more than allowed', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.approve([getAddress(addr1.account.address), value], {account: owner.account});
 
         for (let i = 0; i < value; i++) {
@@ -124,9 +139,9 @@ describe("FoxGhostToken", async function () {
           'ERC20InsufficientAllowance'
         );
     });
-    
+
     it('Test Approve: BurnFrom', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.transfer([getAddress(addr1.account.address), value], {account: owner.account});
         await Token.write.approve([getAddress(owner.account.address), value], {account: addr1.account});
         await Token.write.burnFrom([getAddress(addr1.account.address), value], {account: owner.account});
@@ -134,15 +149,15 @@ describe("FoxGhostToken", async function () {
     });
 
     it('Test Allowance', async function(){
-        const { Token, owner, addr1 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1 } = await deployContract();
         await Token.write.approve([getAddress(addr1.account.address), value], {account: owner.account});
 
         assert.equal(await Token.read.allowance([getAddress(owner.account.address), addr1.account.address]), value);
     });
 
     it('Test 0 address: transfer', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
-        
+        const { Token, owner, addr1, addr2 } = await deployContract();
+
         await viem.assertions.revertWithCustomError(
           Token.write.transfer(['0x0000000000000000000000000000000000000000', value], { account: owner.account.address }),
           Token,
@@ -151,7 +166,7 @@ describe("FoxGhostToken", async function () {
     });
 
     it('Test 0 address: transferFrom: to', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await Token.write.approve([getAddress(addr1.account.address), value], {account: owner.account});
 
         await viem.assertions.revertWithCustomError(
@@ -162,7 +177,7 @@ describe("FoxGhostToken", async function () {
     });
 
     it('Test 0 address: approve: from', async function(){
-        const { Token, owner, addr1, addr2 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1, addr2 } = await deployContract();
         await viem.assertions.revertWithCustomError(
           Token.write.approve(['0x0000000000000000000000000000000000000000', value], {account: owner.account}),
           Token,
@@ -171,12 +186,12 @@ describe("FoxGhostToken", async function () {
     });
 
     it('Test Events: Transfer', async function(){
-        const { Token, owner, addr1 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1 } = await deployContract();
         await viem.assertions.emitWithArgs(Token.write.transfer([getAddress(addr1.account.address), value], {account: owner.account}), Token, 'Transfer', [getAddress(owner.account.address), getAddress(addr1.account.address), value]);
     });
 
     it('Test Events: Approval', async function(){
-        const { Token, owner, addr1 } = await networkHelpers.loadFixture(deployTokenFixture);
+        const { Token, owner, addr1 } = await deployContract();
         await viem.assertions.emitWithArgs(
           Token.write.approve([getAddress(addr1.account.address), value], {account: owner.account}), 
           Token,
